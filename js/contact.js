07ec185a25054fb84509098d4be1a5d12093e167
Michael Mamman <michaelyak66@gmail.com>
2024-09-19 15:25:07 +0100

    progress: contact page done

--- js/contact.js
+++ js/contact.js
@@ -17,23 +17,55 @@
 
     // fail function
     function fail_func(data) {
-        message.fadeIn().removeClass('alert-success').addClass('alert-success');
-        message.text(data.responseText);
+        message.fadeIn().removeClass('alert-success').addClass('alert-danger');
+        message.text(data);
         setTimeout(function () {
             message.fadeOut();
         }, 2000);
     }
     
     form.submit(function (e) {
+        console.log("oyanaaaaaaa")
+        let form_data = $(this).serializeArray();
+
+        // Access each value by name
+        let data = {};
+        form_data.forEach(field => {
+            data[field.name] = field.value;
+        });
+    
+    
+        // Example of accessing specific values
+        let name = data.name;
+        let email = data.email;
+        let phone = data.phone;
+        let subject = data.subject;
+        let message = data.message;
+        const payload = {
+            name: name,
+            subject: subject,
+            message: message,
+            email: email,
+            phone: phone
+        };
         e.preventDefault();
-        form_data = $(this).serialize();
-        $.ajax({
-            type: 'POST',
-            url: form.attr('action'),
-            data: form_data
+        
+        fetch('https://apps.oswallet.app/oyasyncwalletapi/api/WebsiteRequest/SaveEnquiry', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(payload)
         })
-        .done(done_func)
-        .fail(fail_func);
+            .then(response => response.json())
+            .then(data => {
+                done_func('Your message has been sent successfully.')
+            })
+            .catch((error) => {
+                fail_func('There was an error sending your message. Please try again later.')
+            });
+
+
     });
     
 })(jQuery);
